dd1b09800c3350f61a953563c9c931eb465af367
Tim Banta <timbanta@me.com>
2022-05-26 11:20:39 +0800

    Completed 'yelp-mvp' challenge without the front end or refactoring

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+puts "Destroying data"
+  Restaurant.destroy_all
+puts "Starting seeds"
+
+10.times do
+  restaurant = Restaurant.create(name: Faker::Company.name, address: Faker::Address.city, phone_number: Faker::PhoneNumber.phone_number_with_country_code, category: Restaurant::CATEGORY.sample)
+  5.times do
+    Review.create(rating: Review::RATING.sample, content: Faker::Restaurant.review, restaurant: restaurant)
+    puts "#{Review.count} reviews created for #{restaurant.name}"
+  end
+  puts restaurant.name
+end
